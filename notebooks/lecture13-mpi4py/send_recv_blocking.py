b4c0fc185b00efdd3a0c331f2abf7833f2ad1708
Mihai Duta <mcduta@users.noreply.github.com>
2021-11-30 12:03:40 +0000

    Update send_recv_blocking.py

--- notebooks/lecture13-mpi4py/send_recv_blocking.py
+++ notebooks/lecture13-mpi4py/send_recv_blocking.py
@@ -2,13 +2,12 @@
 
 from mpi4py import MPI
 import numpy
-import sys
 
 def main (comm):
     """Send a message between ranks 0 and 1"""
 
     if comm.size != 2:
-        sys.stdout.write ("Only two processes allowed\n")
+        raise ValueError("only two processes allowed")
         comm.Abort(1)
 
     # buffer length
@@ -19,14 +18,22 @@ def main (comm):
     if rank == 0:
         # create send buffer
         buf = numpy.ones (blen, numpy.double)
+        # report
+        print(F" rank 0 sending: {buf}")
         # send buffer
         comm.Send ( [buf, blen, MPI.DOUBLE], dest = 1, tag = 999 )
+        # report
+        print(F" rank 0 sent: {buf}")
+
     elif rank == 1:
         # allocate space for recv buffer
         buf = numpy.empty (blen, numpy.double)
+        # report
+        print(F" rank 1 receives in: {buf}")
         # receive buffer
         comm.Recv ( [buf, blen, MPI.DOUBLE], source = 0, tag = 999 )
-        print " rank 1 received: %s" % (buf)
+        # report
+        print(F" rank 1 received: {buf}")
 
 if __name__ == "__main__":
     main (MPI.COMM_WORLD)
